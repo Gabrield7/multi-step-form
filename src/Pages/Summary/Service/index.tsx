5302550ebff567d72bd7554508ae17c73a62084c
Gabrield7 <gabrieldantas503@gmail.com>
2025-03-09 03:15:57 -0300

    fix: summary styles

--- src/Pages/Summary/Service/index.tsx
+++ src/Pages/Summary/Service/index.tsx
@@ -7,14 +7,30 @@ interface IServiceProps {
 }
 
 export const Service: React.FC<IServiceProps> = (props) => {
+    const colorStyle = (element: string) => {
+        const colorMap: Record<string, string> = {
+            'plan:h3': 'marine-blue',
+            'plan:p': 'marine-blue',
+            'addon:p': 'marine-blue',
+            'addon:h3': 'cool-gray',
+            'total:h3': 'cool-gray',
+            'total:p': 'purplish-blue'
+        };
+    
+        return colorMap[`${props.type}:${element}`] || '';
+    };
 
     return (
         <div className='service__container'>
             <div>
-                <h3>{props.name}</h3>
+                <h3 className={`${colorStyle('h3')}`}>{props.name}</h3>
                 {props.type === 'plan' && <button>Change</button>}
             </div>
-            <p>{`+$${props.price}/mo`}</p>
+            <p 
+                className={`${colorStyle('p')}`}>{`+$${props.price}/mo`}
+            </p>
         </div>
     )
 }
+
+//props.type === 'plan'? 'name-plan':''
