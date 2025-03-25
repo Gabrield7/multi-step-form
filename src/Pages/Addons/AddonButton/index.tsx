51780643605cac5219854d9f9f4327f9d4d0b95d
Gabrield7 <gabrieldantas503@gmail.com>
2025-03-25 17:40:20 -0300

    feat: add addons page validation

--- src/Pages/Addons/AddonButton/index.tsx
+++ src/Pages/Addons/AddonButton/index.tsx
@@ -1,26 +1,31 @@
 import { useState, useContext } from 'react';
 import { availableAddons } from '@contexts/PlanContext/Models';
 import { PlanContext } from '@contexts/PlanContext';
+import { UseFormRegisterReturn } from 'react-hook-form';
 import clsx from 'clsx';
 import './AddonButton.scss';
 
 interface IAddonProps {
     addonName: 'Online service' | 'Larger storage' | 'Customizable profile'
+    register: UseFormRegisterReturn
 }
 
-export const AddonButton: React.FC<IAddonProps> = ({ addonName }) => {
+export const AddonButton: React.FC<IAddonProps> = ({ addonName, register }) => {
     const [hover, setHover] = useState(false);
     const { plan, setAddons } = useContext(PlanContext);
     
     const selectedAddon = availableAddons[addonName];
 
     return (
-        <div className={clsx('addon__container', { 'checked': plan.addons.includes(addonName) || hover })}>
-            <label className='custom-checkbox'>
+        <fieldset className={clsx('addon__container', { 'checked': plan.addons.includes(addonName) || hover })}>
+            <label htmlFor={addonName} className='custom-checkbox'>
                 <input 
                     type='checkbox'
-                    onChange={() => setAddons(addonName)}
+                    id={addonName}
+                    value={addonName}
+                    {...register}
                     checked={plan.addons.includes(addonName)}
+                    onChange={() => setAddons(addonName)}
                 />
                 <span 
                     onMouseEnter={() => setHover(true)}
@@ -36,6 +41,6 @@ export const AddonButton: React.FC<IAddonProps> = ({ addonName }) => {
                 ? `+$${selectedAddon.price.yearly}/yr`
                 : `+$${selectedAddon.price.monthly}/mo`
             }`}</span>
-        </div>
+        </fieldset>
     )
 }
--- src/Pages/Addons/index.tsx
+++ src/Pages/Addons/index.tsx
@@ -1,18 +1,36 @@
 import { BodyPage } from '@components/BodyPage';
 import { AddonButton } from './AddonButton';
+import { useForm } from 'react-hook-form';
+import { PageValidationContext } from '@contexts/PageValidationContext';
+import { useContext } from 'react';
+import { useNavigate } from 'react-router';
 import './Addons.scss';
 
 export const Addons = () => {
+    const { validatePage } = useContext(PageValidationContext);
+    const navigate = useNavigate();
+
+    const { register, handleSubmit } = useForm();
+
+    const onValid = () => {
+        validatePage('/summary', true);
+        navigate('/summary');
+    };
+    
     return(
         <BodyPage 
             title={'Pick add-ons'}
             subtitle={'Add-ons help enhance your game experience.'}
         >
-            <section className='addons__container'>
-                <AddonButton addonName='Online service'/>
-                <AddonButton addonName='Larger storage'/>
-                <AddonButton addonName='Customizable profile'/>
-            </section>
+            <form 
+                id='addons-form' 
+                onSubmit={handleSubmit(onValid)}
+                className='addons__container'
+            >
+                <AddonButton addonName='Online service' register={register('addons')}/>
+                <AddonButton addonName='Larger storage' register={register('addons')}/>
+                <AddonButton addonName='Customizable profile' register={register('addons')}/>
+            </form>
         </BodyPage>
     )
 };
